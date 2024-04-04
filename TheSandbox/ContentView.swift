098aabac3bee47dbb5ae7785f594ad1f809caf56
Bobbyphtr <bobby.pehtrus@traveloka.com>
2024-04-04 15:58:08 +0700

    Initial Commit

---
+++ TheSandbox/ContentView.swift
@@ -0,0 +1,25 @@
+//
+// ContentView.swift
+// TheSandbox
+//
+// Created by Bobby Pehtrus on 04/04/24.
+// Copyright © 2024 Traveloka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheSandbox/TheSandboxApp.swift
@@ -0,0 +1,18 @@
+//
+// TheSandboxApp.swift
+// TheSandbox
+//
+// Created by Bobby Pehtrus on 04/04/24.
+// Copyright © 2024 Traveloka. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TheSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
